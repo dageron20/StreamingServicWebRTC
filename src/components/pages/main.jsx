f5268d473a8037edcb51b55ba3cf3f0fdb6bf069
DAGERON <den-kir1@yandex.ru>
2022-06-13 00:28:41 +0300

    add:aside

--- src/components/pages/main.jsx
+++ src/components/pages/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../header/header';
+import LeftMenu from '../leftMenu/leftMenu';
 import Videocapture from '../videocapture/videocapture';
 
 const Main = () => {
     return (
         <>
             <Header />
+            <LeftMenu />
             <Videocapture />
         </>
     )
